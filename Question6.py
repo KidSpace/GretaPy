347a2d67dc7ed357b89479d2454473e0fb0d2af2
Lawler5 <maijalawler@yahoo.com>
2017-04-17 13:57:41 -0400

    Create Question6.py

---
+++ Question6.py
@@ -0,0 +1,37 @@
+# Greta's Quiz Game
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
+
+number = 1
+
+#--Cool Image to show if they answer the question Right
+pil_im = Image.open('door1.png', 'r')
+imshow(np.asarray(pil_im))
+imshow(pil_im)
+
+#--The Question and Answer Pairs
+print('Sign language, lip reading, and talking are all ways deaf people participate in conversations.\n\n')
+print('a. True\n')
+print('b. False\n')
+
+
+#--Correct Answer
+answer = 'd'
+
+#--Link to learn more
+url = "http://school.eb.com/levels/high/article/deafness/29631"
+
+#--Boolean
+understand_deafness=False
+
+#--While loop
+while not understand_deafness:        
+    guess = input('a, b, c, or d?\n\n\n')
+    
+    if guess == answer:
+        print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number))
+        understand_deafness=True
+    else:
+        print('\nSorry, Try Again!)
+        
