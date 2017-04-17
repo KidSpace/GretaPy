e8175025d1229009e380ee1e450c971a831223eb
Lawler5 <maijalawler@yahoo.com>
2017-04-17 09:46:06 -0400

    Update Question4.py

--- Question4.py
+++ Question4.py
@@ -11,28 +11,28 @@ imshow(np.asarray(pil_im))
 imshow(pil_im)
 
 #--The Question and Answer Pairs
-print('Which of these is he best answer to this question?\n\n')
-print('A. Some Answer to the question\n')
-print('B. A Different Answer to the question\n')
-print('C. A Different Answer to the question\n')
-print('D. A Different Answer to the question\n')
+print('Deaf children do not experience...as a result of their condition.\n\n')
+print('a. frequent pneumonia\n')
+print('b. problems balancing\n')
+print('c. speech delays\n')
+print('d. difficulty communicating\n')
 
 #--Correct Answer
-answer = 'B'
+answer = 'a'
 
 #--Link to learn more
-url = "http://www.specialeducationguide.com/disability-profiles/hearing-impairments/"
+url3 = "http://www.livestrong.com/article/509643-characteristics-of-hearing-impairment-and-deafness-in-children/"
 
 #--Boolean
 understand_deafness=False
 
 #--While loop
 while not understand_deafness:        
-    guess = input('A, B, C, or D?\n\n\n')
+    guess = input('a, b, c, or d?\n\n\n')
     
     if guess == answer:
         print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number))
         understand_deafness=True
     else:
-        print('\nSorry, Try Again! Go Here For a Clue: {}'.format(url))
+        print('\nSorry, Try Again! Go Here For a Clue: {}'.format(url3))
         
