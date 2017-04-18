441f6ca23da710d1baf7bc37a527bbe42c375b79
Lawler5 <maijalawler@yahoo.com>
2017-04-17 20:01:41 -0400

    Update Question10.py

--- Question10.py
+++ Question10.py
@@ -3,25 +3,22 @@ from matplotlib.pyplot import imshow
 import numpy as np
 from PIL import Image
 
-number = 1
+number = 10
 
 #--Cool Image to show if they answer the question Right
-pil_im = Image.open('door1.png', 'r')
+pil_im = Image.open('door10.png', 'r')
 imshow(np.asarray(pil_im))
 imshow(pil_im)
 
 #--The Question and Answer Pairs
-print('Deafness is...?\n\n')
-print('a. Being dead\n')
-print('b. Not being able to hear at all\n')
-print('c. Slight hearing impairment\n')
-print('d. Either not being able to hear at all OR having slight hearing impairment\n')
+print('Deaf kids are...?\n\n')
+print('a. dumb, stupid, hopeless\n')
+print('b. unique, amazing, adaptable, creative, intelligent, kind, special \n')
+print('c. weird, ugly, awkward\n')
+print('d. unfortunate, unlucky, pathetic\n')
 
 #--Correct Answer
-answer = 'd'
-
-#--Link to learn more
-url = "http://school.eb.com/levels/high/article/deafness/29631"
+answer = 'b'
 
 #--Boolean
 understand_deafness=False
@@ -31,8 +28,8 @@ while not understand_deafness:
     guess = input('a, b, c, or d?\n\n\n')
     
     if guess == answer:
-        print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number))
+        print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number) 'And Passed The Challlenge!')
         understand_deafness=True
     else:
-        print('\nSorry, Try Again! Go Here For a Clue: {}'.format(url))
+        print('\nSorry, Try Again! You really should have gotten this one right!)
         
