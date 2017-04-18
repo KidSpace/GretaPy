ec3d6abcf5b3d9b48e61ac12c8e3622093f1aebd
Lawler5 <maijalawler@yahoo.com>
2017-04-17 20:04:25 -0400

    Update Question10.py

--- Question10.py
+++ Question10.py
@@ -28,7 +28,7 @@ while not understand_deafness:
     guess = input('a, b, c, or d?\n\n\n')
     
     if guess == answer:
-        print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number) 'And Passed The Challlenge!')
+        print('Correct! Great Job! You Just Passed Through Door # {}!'.format(number) 'And Completed The Challlenge!')
         understand_deafness=True
     else:
         print('\nSorry, Try Again! You really should have gotten this one right!)
